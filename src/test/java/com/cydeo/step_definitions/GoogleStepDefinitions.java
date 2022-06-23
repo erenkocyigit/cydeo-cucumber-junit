bf9996a78486193775429f39ec8e1a035ab64865
erenkocyigit <m.erenkocyigit@gmail.com>
2022-06-23 16:27:30 +0200

    day 15 login feature

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,11 +1,32 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
 
 public class GoogleStepDefinitions {
+
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+    @When("user types apple and clicks enter")
+    public void user_types_apple_and_clicks_enter() {
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+    }
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+
+        String expectedTitle = "apple - Google Suche";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second actual
+        Assert.assertEquals(expectedTitle,actualTitle);
+
+       // Assert.assertTrue(expectedTitle.equals(actualTitle));
+    }
 
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
